50472b1d823391d53d78b10451f32573b8440a41
sbx_user1051 <sbx_user1051@169.254.75.45>
2023-12-03 18:15:55 +0000

    Sun Dec 03 18:15:55 UTC 2023

---
+++ src/BeFaster.App/Solutions/CHK/GetFreeConditionalSpecialOffer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class GetFreeConditionalSpecialOffer : GetFreeSpecialOffer
+    {
+
+        public override void ApplyOffer(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
