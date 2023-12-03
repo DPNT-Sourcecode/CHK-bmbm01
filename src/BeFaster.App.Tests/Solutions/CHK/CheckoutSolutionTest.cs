26a9b429b4f8bf2beabbe60d767cdb7b9a4d521f
sbx_user1051 <sbx_user1051@169.254.75.45>
2023-12-03 20:04:17 +0000

    Sun Dec 03 20:04:17 UTC 2023

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -6,21 +6,23 @@ namespace BeFaster.App.Tests.Solutions.CHK
     public class CheckoutSolutionTest
     {
         [Theory]
-        [InlineData("1", -1)]
-        [InlineData("ABCD", 115)]
-        [InlineData("AAABBCCDD", 245)]
-        [InlineData("AAAAAAABBBBBCCDD", 490)]
-        [InlineData("AAAAAAABBBBBCCDDE", 530)]
-        [InlineData("AAAAAAABBBBBCCDDEE", 540)]
-        [InlineData("EEB", 80)]
-        [InlineData("EEEB", 120)]
-        [InlineData("EEEEBB", 160)]
-        [InlineData("AAAAAAABBBBBCCDDEEFF", 560)]
-        [InlineData("AAAAAAABBBBBCCDDEEFFF", 560)]
-        [InlineData("AAAAAAABBBBBCCDDEEFFFF", 570)]
-        [InlineData("UUU", 120)]
-        [InlineData("XXXYYYZZZ", 135)]
-        [InlineData("XXXXYYYYZZZ", 169)]
+        //[InlineData("1", -1)]
+        //[InlineData("ABCD", 115)]
+        //[InlineData("AAABBCCDD", 245)]
+        //[InlineData("AAAAAAABBBBBCCDD", 490)]
+        //[InlineData("AAAAAAABBBBBCCDDE", 530)]
+        //[InlineData("AAAAAAABBBBBCCDDEE", 540)]
+        //[InlineData("EEB", 80)]
+        //[InlineData("EEEB", 120)]
+        //[InlineData("EEEEBB", 160)]
+        //[InlineData("AAAAAAABBBBBCCDDEEFF", 560)]
+        //[InlineData("AAAAAAABBBBBCCDDEEFFF", 560)]
+        //[InlineData("AAAAAAABBBBBCCDDEEFFFF", 570)]
+        //[InlineData("UUU", 120)]
+        //[InlineData("XXXYYYZZZ", 135)]
+        //[InlineData("XXXXYYYYZZZ", 169)]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVW", 795)]
+        [InlineData("CXYZYZC", 122)]
         public void ComputePrices_WhenCalled_ShouldReturnPrice(string skus, int expected)
         {
             // Act
@@ -31,4 +33,5 @@ namespace BeFaster.App.Tests.Solutions.CHK
         }
     }
 }
+
 
