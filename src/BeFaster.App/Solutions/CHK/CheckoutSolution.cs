b4a3bc82618834e3b4859377c2a803106c87b48d
sbx_user1051 <sbx_user1051@169.254.75.45>
2023-12-03 18:33:54 +0000

    Sun Dec 03 18:33:54 UTC 2023

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -4,24 +4,9 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        private static readonly Dictionary<char, int> Items = new Dictionary<char, int>()
-        {
-            { 'A', 50 },
-            { 'B', 30 },
-            { 'C', 20 },
-            { 'D', 15 },
-            { 'E', 40 },
-            { 'F', 10 },
-        };
+        private static readonly Dictionary<char, int> Items = GetItems();
 
-        private static readonly List<SpecialOffer> SpecialOffers = new List<SpecialOffer>()
-        {
-            new GetFreeSpecialOffer { Item = 'E', Quantity = 2, FreeItem = 'B', FreeItemQuantity = 1 },
-            new GetFreeConditionalSpecialOffer { Item = 'F', Quantity = 3, FreeItem = 'F', FreeItemQuantity = 1, MinimumQuantity = 3 },
-            new DiscountSpecialOffer { Item = 'A', Quantity = 5, Value = 200 },
-            new DiscountSpecialOffer { Item = 'A', Quantity = 3, Value = 130 },
-            new DiscountSpecialOffer { Item = 'B', Quantity = 2, Value = 45 }
-        };
+        private static readonly List<SpecialOffer> SpecialOffers = ;
 
         public static int ComputePrice(string skus)
         {
@@ -77,5 +62,45 @@ namespace BeFaster.App.Solutions.CHK
                 basket.Price += entry.Value * itemValue;
             }
         }
+
+        private static Dictionary<char, int> GetItems() => new Dictionary<char, int>()
+        {
+            { 'A', 50 },
+            { 'B', 30 },
+            { 'C', 20 },
+            { 'D', 15 },
+            { 'E', 40 },
+            { 'F', 10 },
+            { 'G', 10 },
+            { 'H', 10 },
+            { 'I', 10 },
+            { 'J', 10 },
+            { 'K', 10 },
+            { 'L', 10 },
+            { 'M', 10 },
+            { 'N', 10 },
+            { 'O', 10 },
+            { 'P', 10 },
+            { 'Q', 10 },
+            { 'R', 10 },
+            { 'S', 10 },
+            { 'T', 10 },
+            { 'U', 10 },
+            { 'V', 10 },
+            { 'W', 10 },
+            { 'X', 10 },
+            { 'Y', 10 },
+            { 'Z', 10 },
+        };
+
+        private static List<SpecialOffer> GetSpecialOffers() => new List<SpecialOffer>()
+        {
+            new GetFreeSpecialOffer { Item = 'E', Quantity = 2, FreeItem = 'B', FreeItemQuantity = 1 },
+            new GetFreeConditionalSpecialOffer { Item = 'F', Quantity = 3, FreeItem = 'F', FreeItemQuantity = 1, MinimumQuantity = 3 },
+            new DiscountSpecialOffer { Item = 'A', Quantity = 5, Value = 200 },
+            new DiscountSpecialOffer { Item = 'A', Quantity = 3, Value = 130 },
+            new DiscountSpecialOffer { Item = 'B', Quantity = 2, Value = 45 }
+        };
     }
 }
+
