64752a0f308854dc0c0b565ad903ea3b4fc0c96b
sbx_user1051 <sbx_user1051@169.254.75.45>
2023-12-03 15:48:42 +0000

    Sun Dec 03 15:48:42 UTC 2023

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -4,7 +4,25 @@ namespace BeFaster.App.Solutions.SUM
 {
     public static class SumSolution
     {
-        public static int Sum(int x, int y) => x + y;
+        private static readonly int LowerBound = 0;
+        private static readonly int UpperBound = 100;
+
+        public static int Sum(int x, int y)
+        {
+            CheckValidValue(x);
+            CheckValidValue(y);
+
+            return x + y;
+        }
+
+        private static void CheckValidValue(int value)
+        {
+            if (value < LowerBound || value > UpperBound)
+            {
+                throw new System.Exception("Value is out of range. It should be a number between 0-100.");
+            }
+        }
     }
 }
+
 
