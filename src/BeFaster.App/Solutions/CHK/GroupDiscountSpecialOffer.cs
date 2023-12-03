ad66648909bbdb867af2d688f8eed793720fe7ea
sbx_user1051 <sbx_user1051@169.254.75.45>
2023-12-03 18:56:31 +0000

    Sun Dec 03 18:56:31 UTC 2023

---
+++ src/BeFaster.App/Solutions/CHK/GroupDiscountSpecialOffer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class GroupDiscountSpecialOffer : SpecialOffer
+    {
+        public HashSet<char> ItemGroup { get; set; } = new HashSet<char>();
+
+        public int Price { get; set; }
+
+        public override void ApplyOffer(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
